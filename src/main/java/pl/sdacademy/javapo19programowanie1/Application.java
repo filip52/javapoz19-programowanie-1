ddefd60c9217a5a729872f71440adcdaf9074f78
filip52 <53393033+filip52@users.noreply.github.com>
2019-08-04 10:00:45 +0200

    some change

--- src/main/java/pl/sdacademy/javapo19programowanie1/Application.java
+++ src/main/java/pl/sdacademy/javapo19programowanie1/Application.java
@@ -3,5 +3,7 @@ package pl.sdacademy.javapo19programowanie1;
 public class Application {
     public static void main(String[] args) {
         System.out.println("Witaj swiecie ");
+
+        System.out.println("co tam u cb");
     }
 }
