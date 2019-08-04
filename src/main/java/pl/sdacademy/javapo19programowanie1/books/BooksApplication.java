6d94daaa1e048da7e30ab638a2987c5dec7d9d32
filip52 <53393033+filip52@users.noreply.github.com>
2019-08-04 14:50:55 +0200

    initial structure of application

---
+++ src/main/java/pl/sdacademy/javapo19programowanie1/books/BooksApplication.java
@@ -0,0 +1,7 @@
+package pl.sdacademy.javapo19programowanie1.books;
+
+public class BooksApplication {
+    public static void main(String[] args) {
+        new BooksStart().start();
+    }
+}
---
+++ src/main/java/pl/sdacademy/javapo19programowanie1/books/BooksStart.java
@@ -0,0 +1,41 @@
+package pl.sdacademy.javapo19programowanie1.books;
+
+import java.util.Scanner;
+
+public class BooksStart {
+
+    private BooksViews views;
+
+    public BooksStart() {
+        this.views = new BooksViews(new Scanner(System.in));
+    }
+
+    public void start() {
+         boolean flag = true;
+        do {
+            int decision = views.startMenu();
+            switch (decision){
+                case 1:
+                   authorsView();
+                   break;
+                case 2:
+                    booksView();
+                    break;
+                default:
+                    flag = false;
+            }
+        } while (flag);
+
+    }
+
+
+    private void authorsView() {
+        System.out.println("autorzy");
+    }
+    private void booksView() {
+        System.out.println("ksiazki");
+    }
+
+}
+
+
